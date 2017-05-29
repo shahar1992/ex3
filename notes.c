903a0702dcca8d52ec7bf89f2f8248208020909f
shahar1992 <shahar92@campus.technion.ac.il>
2017-05-29 18:53:39 +0300

    you welcome

---
+++ notes.c
@@ -0,0 +1,90 @@
+/*
+ *
+ * system:
+ *      @functions:
+ *          create system.
+ *          destroy system.
+ *          check validate.
+ *          check mail.
+ *          find company (by mail).
+ *          find recommended room.
+ *          printing functions.
+ *
+ *
+ * Company:
+ *      @functions:
+ *          check validate.
+ *          create company.
+ *          cmp company.
+ *          free company.
+ *          add room.
+ *          remove room.
+ *          get mail.
+ *          find room.
+ *          is any order exist.
+ *          get faculty.
+ *
+ * Rooms:
+ *      @functions:
+ *          check.
+ *          create.
+ *          cmp.
+ *          free.
+ *          check availability.
+ *          add order.
+ *          remove order.
+ *          get id.
+ *          is any order exist.
+ *          get faculty.
+ *
+ *
+ * Orders:
+ *      @functions:
+ *          check.
+ *          create.
+ *          free.
+ *          copy.
+ *          get faculty.
+ *
+ * Escaper:
+ *      @functions:
+ *          check.
+ *          create.
+ *          free.
+ *          copy.
+ *          get mail.
+ *          add order.
+ *          remove order.
+ *          remove all orders.
+ *          get faculty.
+ *          check availability.
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
