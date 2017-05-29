55aa880a4e0c110d78fa8223b6bee9eb6a6373b3
shahar1992 <shahar92@campus.technion.ac.il>
2017-05-29 17:38:11 +0300

    you welcome

---
+++ EscapeTechnion.h
@@ -0,0 +1,12 @@
+#ifndef EX3_ESCAPETECHNION_H
+#define EX3_ESCAPETECHNION_H
+
+#include <stdbool.h>
+
+#include "list.h"
+#include "set.h"
+#include "company.h"
+#include "rooms.h"
+#include "order.h"
+
+#endif //EX3_ESCAPETECHNION_H
