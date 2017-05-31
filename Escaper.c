6f026d0820d248e2202c79cf239d44cfe9ba7cc8
saavivi <saavivi@campus.technion.ac.il>
2017-05-31 17:11:58 +0300

    some changes in orders+escaper

--- Escaper.c
+++ Escaper.c
@@ -1,40 +1,50 @@
 //
 // Created by Amir on 31/05/2017.
 //
+
+/**====================Include files==============================*/
 #include "Escaper.h"
 #include <stdlib.h>
 #include <assert.h>
 #include <string.h>
+/**================================================================*/
+
+/**====================Main entity decleration==============================*/
+struct Escaper_t{
+    char * Email;
+    TechnionFaculty faculty;
+    int skill_level;
+    Order* Orders;
+};
+/**====================end of entity decleration==============================*/
 
 
-/** List of static functions and its usage
- *
- * MailCheck          -checks if a given mail is valid;
- * Facultycheck       -checks if a given faculty is valid;
- *
- *
- *
- */
+
+/**======================Macros===================================*/
 
 #define MEMORY_CHECK_NULL(ptr) \
     if ((ptr) == NULL) { \
         return Escaper_OUT_OF_MEMORY; \
     }
-
-static bool MailCheck(char *mail){
-
-}
-static bool FacultyCheck(TechnionFaculty faculty){
-
-}
-
-struct Escaper_t{
-    char * Email;
-    TechnionFaculty faculty;
-    int skill_level;
-};
+/**====================End of Macros==============================*/
 
 
+
+
+/**=====================Static functions declarations=========================*/
+/** List of static functions and its usage
+ * MailCheck          -checks if a given mail is valid;
+ * Facultycheck       -checks if a given faculty is valid;
+ */
+static bool MailCheck(char *mail);
+static bool FacultyCheck(TechnionFaculty faculty);
+
+/**===================End of static function declarations.====================*/
+
+/**===================Escaper ADT functions implementation====================*/
+
+
+/**============EscaperCreate===========================*/
 Escaper EscaperCreate(char* email, TechnionFaculty faculty, int skill_level){
     if( !MailCheck(email) || !FacultyCheck(faculty) ){
         return NULL;
@@ -42,15 +52,16 @@ Escaper EscaperCreate(char* email, TechnionFaculty faculty, int skill_level){
     Escaper escaper=malloc(sizeof(*escaper));
     MEMORY_CHECK_NULL(escaper);
     escaper->Email= malloc(sizeof(char)*(strlen(email)+1));
-    MEMORY_CHECK_NULL(escaper->Email);
+    if(escaper->Email==NULL){
+
+    }
     strcpy(escaper->Email,email);
     escaper->faculty=faculty;
     escaper->skill_level=skill_level;
 return escaper;
 }
 
-
-
+/**=====EscaperDestroy==================================*/
 void EscaperDestroy(Escaper escaper){
     assert(escaper);
     free(escaper->Email);
@@ -58,7 +69,7 @@ void EscaperDestroy(Escaper escaper){
     return;
 }
 
-
+/**=====EscaperCopy==================================*/
 Escaper EscaperCopy(Escaper escaper){
     assert(escaper);
     Escaper  new_escaper=malloc(sizeof(*new_escaper));
@@ -71,3 +82,43 @@ Escaper EscaperCopy(Escaper escaper){
     return new_escaper;
 }
 
+/**=====EscaperCopy==================================*/
+bool EscaperCmp(Escaper escaper1,Escaper escaper2){
+    assert(escaper1&&escaper2);
+    return (strcmp(escaper1->Email,escaper2->Email));
+    /**strcmp returns 0 if both string are equal which differs
+     * escapers*/
+}
+/**=====EscaperGetEmail==================================*/
+EscaperResult EscaperGetEmail(Escaper escaper,char* mail){
+    assert(escaper&&mail);
+    mail=malloc(sizeof(char)*(strlen(escaper->Email)+1) );
+    MEMORY_CHECK_NULL(mail);
+    strcpy(escaper->Email,mail);
+    return Escaper_SUCCESS;
+}
+
+/**
+ *
+ * Implement order ADT AND CONTINUE
+ *
+ *
+ */
+/**=====EscaperGetOrder=========================*/
+EscaperResult EscaperGetOrder(Escaper escaper,Order order){
+
+}
+
+/** ===============Static functions implementaion==========================*/
+static bool MailCheck(char* mail){
+    char* ptr=mail;
+    int counter =0;
+    while(*ptr!=0){
+        if(*ptr=='@') counter++;
+    }
+    return (counter==1)?TRUE:FALSE;//return true if only 1 @;
+
+}
+static bool FacultyCheck(TechnionFaculty faculty){
+
+}
--- Order.c
+++ Order.c
@@ -2,3 +2,70 @@
 // Created by Amir on 31/05/2017.
 //
 
+/**====================Include files==============================*/
+#include "Order.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+/**================================================================*/
+
+/**====================Main entity decleration==============================*/
+struct Order_t{
+    char * Email;
+    TechnionFaculty faculty;
+    int ID;
+    long start_hour;
+    long days_left;
+    int num_ppl;
+};
+/**====================end of entity decleration==============================*/
+
+/**======================Macros===================================*/
+
+#define MEMORY_CHECK_NULL(ptr) \
+    if ((ptr) == NULL) { \
+        return Order_OUT_OF_MEMORY; \
+    }
+/**====================End of Macros==============================*/
+
+/**=====================Static functions declarations=========================*/
+/** List of static functions and its usage
+ * GetTime          -gets time in time format and returns both wanted hour and
+ *                   number of days left in pointers;
+ */
+static OrderResult GetTime(char *time_format,int* hour,int* days_left);
+
+
+/**===================End of static function declarations.====================*/
+
+/**===================Order ADT functions implementation====================*/
+
+
+/**============OrderCreate===========================*/
+
+Order OrderCreate(char* email, TechnionFaculty faculty, int id,char* time,int num_of_ppl){
+    assert(mail!=NULL && time!=NULL);
+    Order order=malloc(sizeof(*order));
+    MEMORY_CHECK_NULL(order);
+    order->Email=malloc(sizeof(char)*(strlen(email)+1));
+    if(order->Email==NULL){
+
+    }
+
+}
+
+
+/** ===============Static functions implementaion==========================*/
+
+static OrderResult GetTime(char *time_format,long* hour,long* days_left){
+    assert(time_format&&hour&&days_left);
+    char *ptr;
+    long ret;
+    ret = strtol(time_format, &ptr, 10);
+    //printf("number of days is %ld\n", ret);
+    *days_left=ret;
+    ret=strtol(ptr+1,&ptr,10);
+    *hour=ret;
+    //printf("hour is %ld\n", ret);
+}
