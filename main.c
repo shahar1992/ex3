d8237f10f3ed22b325d66f27e6b3526c790fa283
saavivi <saavivi@campus.technion.ac.il>
2017-06-15 17:55:25 +0300

    parser 1

--- main.c
+++ main.c
@@ -2,17 +2,19 @@
 #include "EscapeTechnion.h"
 #include <stdbool.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include "parser.c"
 
 int main(int argc,  char** argv) {
     FILE *input_c,*output_c;
     char* array[] = {argv[0],"-i","input.txt","-o","output.txt"};
-    //char* array1[] = {"-i","input.txt","-o","output.txt"};
-   // MtmErrorCode result;
-    //result = getChannels2(5, array, &input_c, &output_c);
-    //assert(result != MTM_CANNOT_OPEN_FILE);
-   // assert(result != MTM_INVALID_COMMAND_LINE_PARAMETERS);
+    char* array1[] = {"-i","input.txt","-o","output.txt"};
+    MtmErrorCode result;
+    result = getChannels2(5, array, &input_c, &output_c);
+    assert(result != MTM_INVALID_COMMAND_LINE_PARAMETERS);
+    assert(result != MTM_CANNOT_OPEN_FILE);
+
     return 0;
 }
 
