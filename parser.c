bd76f963aa408af76f73cc2868b51a9f8323e7f4
saavivi <saavivi@campus.technion.ac.il>
2017-06-17 19:16:37 +0300

    parser changed

--- parser.c
+++ parser.c
@@ -18,16 +18,19 @@ MtmErrorCode handleCompanyCommand(EscapeTechnion sys,
                                   char* sub_command,char**arg_Array);
 MtmErrorCode handleRoomCommand(EscapeTechnion sys,
                                char* sub_command,char**arg_Array);
+
 MtmErrorCode handleEscaperCommand(EscapeTechnion sys,
                                   char* sub_command,char**arg_Array);
+/*
 MtmErrorCode handleReportCommand(EscapeTechnion system, char* sub_command,
                                  const FILE* output_c);
-static MtmErrorCode reportDay(EscapeTechnion system, const FILE* output_c);
+        static MtmErrorCode reportDay(EscapeTechnion system, const FILE* output_c);
 static MtmErrorCode reportBest(EscapeTechnion system, const FILE* output_c);
 static MtmErrorCode orderPrint(EscapeTechnion system, Order order,
                                FILE* output_c);
+                               */
 
-
+static MtmErrorCode ConvertResult(EscapeTechnionResult result);
 /**-------------------- Command Interpreter-----------------------------------*/
 
 MtmErrorCode parserAnalyzeCommand(EscapeTechnion system, char* buffer,
@@ -65,36 +68,95 @@ MtmErrorCode handleCommand(EscapeTechnion system,
     if(strcmp(command,"company")==0){
         return handleCompanyCommand(system,subcomand,arg_Array);
     }
-    /*
+
     if(strcmp(command,"room")==0){
-        return handleRoomCommand;
+        return handleRoomCommand(system,subcomand,arg_Array);
     }
     if(strcmp(command,"escaper")==0){
-        return handleEscaperommand;
+        return handleEscaperCommand(system,subcomand,arg_Array);
     }
+    /*
     if(strcmp(command,"report")==0){
-        return handleReportommand;
-    }
+        return handleReportommand(system,subcomand,arg_Array);
+    }*/
     return MTM_INVALID_COMMAND_LINE_PARAMETERS;
-     */
-    return MTM_SUCCESS;
 }
 
 MtmErrorCode handleCompanyCommand(EscapeTechnion system,
                                   char* subcomand,char**arg_Array){
+    EscapeTechnionResult result;
     if(strcmp(subcomand,"add")==0){
         char *ptr;
-        long faculty;
-        strtol(arg_Array[1],&ptr,10);
-        return escapeTechnionAddCompany(system,arg_Array[0],faculty);
+        long faculty=strtol(arg_Array[1],&ptr,10);
+        result=escapeTechnionAddCompany(system,arg_Array[0],faculty);
+         return ConvertResult(result);
     }
+    if(strcmp(subcomand,"remove")==0){
+        result=escapeTechnionRemoveCompany(system,arg_Array[0]);
+         return ConvertResult(result);
+    }
+    return MTM_INVALID_COMMAND_LINE_PARAMETERS;
 }
 
 MtmErrorCode handleRoomCommand(EscapeTechnion system,
-                               char* sub_command,char**arg_Array);
-MtmErrorCode handleEscaperCommand(EscapeTechnion system,
-                                  char* sub_command,char**arg_Array);
+                               char* sub_command,char**arg_Array){
+    EscapeTechnionResult result;
+    if(strcmp(sub_command,"add")==0){
+        char *ptr;
+        long id=strtol(arg_Array[1],&ptr,10);
+        long price=strtol(arg_Array[2],&ptr,10);
+        long num_ppl=strtol(arg_Array[3],&ptr,10);
+        long open_hour=strtol(arg_Array[4],&ptr,10);
+        ptr++;
+        long close_hour=strtol(ptr,&(ptr),10);
+        long difficulty=strtol(arg_Array[5],&ptr,10);
+        result=escapeTechnionAddRoom(system,arg_Array[0],id,price,num_ppl,open_hour,close_hour,difficulty);
+        return ConvertResult(result);
+    }
+    if(strcmp(sub_command,"remove")==0){
+        char *ptr;
+        long faculty=strtol(arg_Array[0],&ptr,10);
+        long id=strtol(arg_Array[1],&ptr,10);
+        result=escapeTechnionRemoveRoom(system,faculty,id);
+        return ConvertResult(result);
+    }
+    return MTM_INVALID_COMMAND_LINE_PARAMETERS;
+}
 
+MtmErrorCode handleEscaperCommand(EscapeTechnion system,
+                                  char* sub_command,char**arg_Array){
+    EscapeTechnionResult result;
+    if(strcmp(sub_command,"add")==0){
+        char *ptr;
+        long faculty=strtol(arg_Array[1],&ptr,10);
+        long skill_level=strtol(arg_Array[2],&ptr,10);
+        result=escapeTechnionAddClient(system,arg_Array[0],faculty,skill_level);
+        return ConvertResult(result);
+    }
+    if(strcmp(sub_command,"remove")==0){
+        result=escapeTechnionRemoveClient(system,arg_Array[0]);
+        return ConvertResult(result);
+    }
+    if(strcmp(sub_command,"order")==0){
+        char *ptr;
+        long faculty=strtol(arg_Array[1],&ptr,10);
+        long id=strtol(arg_Array[2],&ptr,10);
+        long day=strtol(arg_Array[3],&ptr,10);
+        ptr++;
+        long hour=strtol(ptr,&(ptr),10);
+        long num_ppl=strtol(arg_Array[4],&ptr,10);
+        result=escapeTechnionAddOrder(system,arg_Array[0],faculty,id,day,hour,num_ppl);
+        return ConvertResult(result);
+    }
+    if(strcmp(sub_command,"recommend")==0){
+        char *ptr;
+        long num_ppl=strtol(arg_Array[1],&ptr,10);
+        result=escapeTechnionRecommendedRoomOrder(system,arg_Array[0],num_ppl);
+        return ConvertResult(result);
+    }
+    return MTM_INVALID_COMMAND_LINE_PARAMETERS;
+}
+/*
 MtmErrorCode handleReportCommand(EscapeTechnion system, char* sub_command,
                                  const FILE* output_c) {
     assert(system);
@@ -108,7 +170,7 @@ MtmErrorCode handleReportCommand(EscapeTechnion system, char* sub_command,
     return result;
 }
 
-static MtmErrorCode reportDay(EscapeTechnion system,const FILE* output_c){
+static MtmErrorCode reportDay(EscapeTechnion system, FILE* output_c){
     assert(system);
     FILE* output_channel = output_c;
     int day = escapeTechnionGetDay(system);
@@ -150,4 +212,37 @@ static MtmErrorCode orderPrint(EscapeTechnion system,Order order,
     company_email,room_faculty,roomGetId(room),orderGetHour(order),
                   orderGetDifficulty(order),orderGetNumOfPeople(order),price);
     return MTM_SUCCESS;
+}
+
+*/
+static MtmErrorCode ConvertResult(EscapeTechnionResult result){
+    switch(result){
+        case ESCAPE_TECHNION_SUCCESS:
+            return MTM_SUCCESS;
+        case ESCAPE_TECHNION_ROOM_NOT_AVAILABLE:
+            return MTM_ROOM_NOT_AVAILABLE;
+        case ESCAPE_TECHNION_NO_ROOMS_AVAILABLE:
+            return MTM_NO_ROOMS_AVAILABLE;
+        case ESCAPE_TECHNION_CLIENT_EMAIL_DOES_NOT_EXIST:
+            return MTM_CLIENT_EMAIL_DOES_NOT_EXIST;
+        case ESCAPE_TECHNION_CLIENT_IN_ROOM:
+            return MTM_CLIENT_IN_ROOM;
+        case ESCAPE_TECHNION_ID_DOES_NOT_EXIST:
+            return MTM_ID_DOES_NOT_EXIST;
+        case ESCAPE_TECHNION_INVALID_PARAMETER:
+            return MTM_INVALID_PARAMETER;
+        case ESCAPE_TECHNION_NULL_PARAMETER:
+            return MTM_NULL_PARAMETER;
+        case ESCAPE_TECHNION_EMAIL_ALREADY_EXISTS:
+            return MTM_EMAIL_ALREADY_EXISTS;
+        case ESCAPE_TECHNION_ID_ALREADY_EXIST:
+            return MTM_ID_ALREADY_EXIST;
+        case ESCAPE_TECHNION_COMPANY_EMAIL_DOES_NOT_EXIST:
+            return MTM_COMPANY_EMAIL_DOES_NOT_EXIST;
+        case ESCAPE_TECHNION_RESERVATION_EXISTS:
+            return MTM_RESERVATION_EXISTS;
+        case ESCAPE_TECHNION_OUT_OF_MEMORY:
+            return MTM_OUT_OF_MEMORY;
+
+    }
 }
